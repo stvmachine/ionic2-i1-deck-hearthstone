adda230ed341bb0dd648360dbe80dad608a0a2f6
estvmachine <campos.esteban@gmail.com>
2017-04-12 17:42:02 -0300

    Se separa heroes a una constante

---
+++ src/services/heroes.ts
@@ -0,0 +1,11 @@
+export const ClassesHeroes=[
+  { className: 'warrior', image: '/images/garrosh.png' },
+  { className: 'shaman', image: '/images/thrall.png' },
+  { className: 'rogue', image: '/images/valeera.png' },
+  { className: 'paladin', image: '/images/uther.png' },
+  { className: 'hunter', image: '/images/rexxar.png' },
+  { className: 'druid', image: '/images/malfurion.png' },
+  { className: 'warlock', image: '/images/guldan.png' },
+  { className: 'mage', image: '/images/jaina.png' },
+  { className: 'priest', image: '/images/anduin.png' }
+];
