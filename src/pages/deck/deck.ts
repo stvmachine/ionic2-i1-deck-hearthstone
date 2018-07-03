5480cae78ade9be1061d292ed84c8ff34204ce65
stevmachine <campos.esteban@gmail.com>
2018-07-04 02:10:51 +1000

    added selectors for add/remove cards

--- src/pages/deck/deck.ts
+++ src/pages/deck/deck.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController,NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { CardRestService } from '../../services/cardRest.service';
 
 @Component({
@@ -8,11 +8,11 @@ import { CardRestService } from '../../services/cardRest.service';
 })
 export class DeckPage {
 
-  classNameUser:string;
-  classCards: Array<any>= [];
-  neutralCards: Array<any>= [];
+  classNameUser: string;
+  classCards: Array<any> = [];
+  neutralCards: Array<any> = [];
 
-  choosenCards: Array<any>= [];
+  choosenCards: Array<any> = [];
 
   cards: string;
 
@@ -21,25 +21,47 @@ export class DeckPage {
     public params: NavParams,
     public cardRest: CardRestService) {
 
-      this.classNameUser= this.params.get('className');
+    this.classNameUser = this.params.get('className');
 
-      this.cards='card_class';
+    this.cards = 'card_class';
 
-      this.cardRest.getCards().subscribe(response=>{
-          let data= JSON.parse(response._body);
-          this.classCards= data.cards.filter(c=> c.hero=== this.classNameUser).sort((n1,n2) => n1.mana - n2.mana).map((c)=>{ c.count=0; return c;});
-          this.neutralCards= data.cards.filter(c=> c.hero=== 'neutral').sort((n1,n2) => n1.mana - n2.mana).map((c)=>{ c.count=0; return c;});
-      });
-    }
+    this.cardRest.getCards().subscribe(response => {
+      let data = JSON.parse(response._body);
+      this.classCards = data.cards.filter(c => c.hero === this.classNameUser)
+        .sort((n1, n2) => n1.mana - n2.mana)
+        .map((c) => { c.count = 0; return c; });
+      this.neutralCards = data.cards.filter(c => c.hero === 'neutral')
+        .sort((n1, n2) => n1.mana - n2.mana)
+        .map((c) => { c.count = 0; return c; });
+    });
+  }
 
-    addDeck(card, isNeutral:boolean= false){
-      let plusCard = isNeutral ?  this.neutralCards.find(c=>c.id=== card.id) : this.classCards.find(c=>c.id=== card.id);
+  addDeck(card) {
+    let plusCard = card.hero === 'neutral' ? this.neutralCards.find(c => c.id === card.id) : this.classCards.find(c => c.id === card.id);
+    if (plusCard.count < 2) {
       plusCard.count++; //llamado por referencia
 
-      if(!this.choosenCards.find(c=>c.id === card.id)){
+      if (!this.choosenCards.find(c => c.id === card.id)) {
         this.choosenCards.push(plusCard);
-        this.choosenCards= this.choosenCards.sort((n1,n2) => n1.mana - n2.mana);
+        this.choosenCards = this.choosenCards.sort((n1, n2) => n1.mana - n2.mana);
       }
     }
+  }
+
+  removeDeck(card) {
+    let plusCard = card.hero === 'neutral' ? this.neutralCards.find(c => c.id === card.id) : this.classCards.find(c => c.id === card.id);
+
+    if (plusCard.count > 0) {
+      plusCard.count--;
+
+      if (plusCard.count === 0) {
+        const indexCard = this.choosenCards.findIndex(c => c.id === card.id);
+        if (indexCard > -1) {
+          this.choosenCards.splice(indexCard , 1);
+        }
+
+      }
+    }
+  }
 
 }
